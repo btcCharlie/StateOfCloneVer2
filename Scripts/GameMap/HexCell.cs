6523f9b1f41f3772809bb3d7a611f0be73a07e4a
btCharlie <thomas.b.charlie@gmail.com>
2023-12-24 12:49:42 +0100

    initial commit

---
+++ Scripts/GameMap/HexCell.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+namespace StateOfClone
+{
+    public class HexCell
+    {
+
+    }
+}
---
+++ Scripts/GameMap/HexCoordinates.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+namespace StateOfClone
+{
+    public class HexCoordinates
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+
+    }
+}
